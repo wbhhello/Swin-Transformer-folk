19844cdd04cf9c566b4b49f0842890edc2d60ec8
funnyWang <1353264157@qq.com>
2023-11-02 17:17:38 +0800

    自定义加载数据集

---
+++ timm/dateLoader.py
@@ -0,0 +1,72 @@
+import cv2
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+
+data_transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # 根据Swin Transformer的模型选择适当的输入尺寸
+    transforms.ToTensor(),
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+
+class CustomDataset(Dataset):
+    def __init__(self, img_dir, labels_file, transform=data_transform):
+        self.img_dir = img_dir
+        self.labels_file = labels_file
+        self.img_names = os.listdir(img_dir)  # 读取文件夹中的所有图片名称
+        self.labels = [int(line.strip()) for line in open(labels_file, 'r').readlines()]  # 读取标签文件
+        self.labels = torch.tensor(self.labels)  # 将标签转换为张量
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_names)
+
+    def __getitem__(self, idx):
+        img_name = self.img_names[idx]
+        img_path = os.path.join(self.img_dir, img_name)  # 拼接图片路径
+        # 读取JPEG图片
+        img = Image.open(img_path)
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+        label = self.labels[idx]  # 获取标签
+        img = self.transform(img)
+        # print(f'label{label},image{img}')
+        return img, label
+
+
+# 假设你的数据集为custom_dataset，包含所有的图像和标签
+def getData(data_list: tuple, transform):
+    custom_dataset = CustomDataset(data_list[0], data_list[1], transform)
+    total_size = len(custom_dataset)
+    train_size = int(0.9 * total_size)
+    test_size = total_size - train_size
+
+    train_dataset, test_dataset = random_split(custom_dataset, [train_size, test_size])
+    return train_dataset, test_dataset
+
+
+def getDataLoader(data_path=None, transform=None):
+    data_path = data_path if data_path else (
+        '../checkpoint/ILSVRC2012_img_val', '../checkpoint/ILSVRC2012_validation_ground_truth.txt')
+    train_dataset, test_dataset = getData(data_path, transform)
+    batch_size = 16  # 设置适当的批量大小
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    # for img, label in test_dataloader:
+    #     print(label)
+    print(len(test_dataloader))  # 313
+    print(len(train_dataloader))  # 2813
+    return train_dataloader, test_dataloader
+
+
+def main():
+    getDataLoader(transform=data_transform)
+
+
+if __name__ == main():
+    main()
---
+++ timm/swin.py
@@ -0,0 +1,63 @@
+import torch
+import timm
+import torchvision.transforms as transforms
+from timm.data import resolve_data_config
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from dateLoader import getDateLoader
+import torch.nn as nn
+import torch.utils as utils
+import torch.optim as optim
+from timm.data.transforms_factory import create_transform
+
+custom_transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # 根据Swin Transformer的模型选择适当的输入尺寸
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+model = timm.create_model('swin_tiny_patch4_window7_224', pretrained=True)
+
+# Create Transform
+transform = create_transform(**resolve_data_config(model.pretrained_cfg, model=model))
+train_dataLoader, test_dataLoader = getDateLoader(transform=transform)
+criterion = nn.CrossEntropyLoss()  # 选择适当的损失函数
+optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.9)  # 选择适当的优化器和学习率
+num_epochs = 10  # 设置适当的训练轮数
+
+
+def train():
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+
+        for images, labels in tqdm(train_dataLoader):
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        # 打印每个训练轮次的损失
+        print(f'Epoch [{epoch + 1}/{num_epochs}] Loss: {running_loss / len(train_dataLoader)}')
+        validate()
+
+    print('Training Finished')
+
+
+def validate():
+    correct = 0
+    total = 0
+    model.eval()
+    with torch.no_grad():
+        for images, labels in tqdm(test_dataLoader):
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print(f'Accuracy on the test dataset: {accuracy:.2f}%')
